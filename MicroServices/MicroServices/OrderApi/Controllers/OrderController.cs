74fe0a1cc193adb2f08a5862d1b8abd330def226
Furkan Güngör <47147484+furkandeveloper@users.noreply.github.com>
2019-09-29 17:16:32 +0300

    Add files via upload

---
+++ MicroServices/MicroServices/OrderApi/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetOrders()
+        {
+            return Ok(new string[] { "Product Id : 5 Price : 20" });
+        }
+    }
+}
---
+++ MicroServices/MicroServices/ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            return Ok(new string[] { "Samsung Galaxy Note 10 , Iphone 7 Plus , Xioami Redmi Note 7" });
+        }
+    }
+}
